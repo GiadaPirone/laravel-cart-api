4d065d96c670bc163ea382fd5241e179954c7ef9
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2024-01-27 15:14:43 +0100

    cart item routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/cart',[CartItemController::class, 'createCart']);
+Route::post('/cart/{cartId}/item/{itemId}', [CartItemController::class, 'addToCart']);
+Route::delete('/cart/{cartId}/item/{itemId}', [CartItemController::class, 'deleteFromCart']);
+Route::get('/cart', [CartItemController::class, 'getAllCarts']);
