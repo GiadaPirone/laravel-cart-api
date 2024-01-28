597914d3beee85fd9c37b541495e74ce67779ef8
GiadaPirone <123646621+GiadaPirone@users.noreply.github.com>
2024-01-28 17:25:33 +0100

    createCart: bad request

--- app/Http/Controllers/CartItemController.php
+++ app/Http/Controllers/CartItemController.php
@@ -4,30 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Models\Cart;
 use App\Models\Item;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class CartItemController extends Controller
 {
-
     public function createCart(Request $request)
     {
+        try {
+            // Cerca l'item nel database
+            $item = Item::findOrFail($request->input('itemId'));
 
-        $cart = Cart::create();
-        $item = Item::findOrFail($request->input('itemId'));
+            // Se l'item esiste, crea il carrello
+            $cart = Cart::create();
+            $cart->items()->attach($item);
 
-        $cart->items()->attach($item);
-        $updatedCart = Cart::with('items')->findOrFail($cart->id);
+            $updatedCart = Cart::with('items')->findOrFail($cart->id);
 
-        // Registra un messaggio nel file di log
-        Log::channel('cart_log')->info('Carrello creato: ' . json_encode($cart));
+            // Registra un messaggio nel file di log
+            Log::channel('cart_log')->info('Carrello creato: ' . json_encode($cart));
 
+            return response()->json([
+                'data' => $updatedCart,
+            ], 201);
 
-        return response()->json([
-
-            'data' => $updatedCart,
-
-        ], 201);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['error' => 'Cart non creato: item inserito non esiste.'], 404);
+        }
     }
 
     public function addToCart($cartId, $itemId)
@@ -40,7 +44,7 @@ class CartItemController extends Controller
         $updatedCart = Cart::with('items')->findOrFail($cart->id);
 
         // Registra un messaggio nel file di log dedicato alle modifiche del carrello
-        Log::channel('cart_modification_log')->info('Prodotto con ID ' . $itemId . ' aggiunto al carrello:'. $cartId . json_encode($updatedCart));
+        Log::channel('cart_modification_log')->info('Prodotto con ID ' . $itemId . ' aggiunto al carrello:' . $cartId . json_encode($updatedCart));
 
         return response()->json([
             'data' => $updatedCart,
@@ -54,7 +58,7 @@ class CartItemController extends Controller
         $cart->items()->updateExistingPivot($itemId, ['deleted_at' => now()]);
 
         // Registra un messaggio nel file di log dedicato alle modifiche del carrello
-        Log::channel('cart_modification_log')->info('Prodotto con ID ' . $itemId . ' rimosso dal carrello:'. $cartId . json_encode($cart));
+        Log::channel('cart_modification_log')->info('Prodotto con ID ' . $itemId . ' rimosso dal carrello:' . $cartId . json_encode($cart));
 
         return response()->json([], 204);
     }
